cbd2de83e69d1e503da03d2bc575603d5e41283f
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-05-15 13:25:49 -0700

    Add tests and a set_number method.

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -8,11 +8,15 @@ defmodule Sequence.Server do
     GenServer.start_link(__MODULE__, current_number, name: __MODULE__)
   end
 
+  def set_number(number) do
+    GenServer.cast __MODULE__, {:set_number, number}
+  end
+
   def next_number do
     GenServer.call __MODULE__, :next_number
   end
 
-  def increment_number(delta) do
+  def increment_number(delta \\ 1) do
     GenServer.cast __MODULE__, {:increment_number, delta}
   end
 
@@ -20,11 +24,15 @@ defmodule Sequence.Server do
   # GenServer implementation
 
   def handle_call(:next_number, _from, current_number) do
-    { :reply, current_number, current_number+1 }
+    {:reply, current_number, current_number+1 }
   end
 
   def handle_cast({:increment_number, delta}, current_number) do
-    { :noreply, current_number + delta}
+    {:noreply, current_number + delta}
+  end
+
+  def handle_cast({:set_number, number}, _current_number) do
+    {:noreply, number}
   end
 
   def format_status(_reason, [ _pdict, state ]) do
--- test/sequence_test.exs
+++ test/sequence_test.exs
@@ -1,8 +1,25 @@
 defmodule SequenceTest do
   use ExUnit.Case
-  doctest Sequence
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  setup do
+    Sequence.Server.set_number(1)
+  end
+
+  test "starts at 1" do
+    assert Sequence.Server.next_number == 1
+  end
+
+  test "increment by 1" do
+    assert Sequence.Server.next_number == 1
+    assert Sequence.Server.next_number == 2
+    assert Sequence.Server.next_number == 3
+  end
+
+  test "increment by an amount" do
+    Sequence.Server.increment_number(10)
+    assert Sequence.Server.next_number == 11
+
+    Sequence.Server.increment_number(3)
+    assert Sequence.Server.next_number == 15
   end
 end

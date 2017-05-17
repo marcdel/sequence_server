dee7010be8245f36ae630495fda57b99655ce6f8
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-05-16 21:51:16 -0700

    Register top level module and give it env variables.

--- lib/sequence.ex
+++ lib/sequence.ex
@@ -2,6 +2,7 @@ defmodule Sequence do
   use Application
 
   def start(_type, _args) do
-    {:ok, _pid} = Sequence.Supervisor.start_link(123)
+    initial_value = Application.get_env(:sequence, :initial_value)
+    {:ok, _pid} = Sequence.Supervisor.start_link(initial_value)
   end
 end
--- lib/sequence/sub_supervisor.ex
+++ lib/sequence/sub_supervisor.ex
@@ -4,6 +4,7 @@ defmodule Sequence.SubSupervisor do
   def start_link(stash_pid) do
     {:ok, _pid} = Supervisor.start_link(__MODULE__, stash_pid)
   end
+
   def init(stash_pid) do
     child_processes = [ worker(Sequence.Server, [stash_pid]) ]
     supervise child_processes, strategy: :one_for_one

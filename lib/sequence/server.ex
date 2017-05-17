8ad90de70ba6326178474077c0864ddc14c25f23
Marc Delagrammatikas <delagrammatikas@gmail.com>
2017-05-17 16:13:44 -0700

    Add code_change version and bump application version.

--- lib/sequence/server.ex
+++ lib/sequence/server.ex
@@ -1,5 +1,6 @@
 defmodule Sequence.Server do
   use GenServer
+  require Logger
 
   @vsn "1"
   defmodule State do
@@ -29,30 +30,44 @@ defmodule Sequence.Server do
   def init(stash_pid) do
     current_number = Sequence.Stash.get_value stash_pid
     {:ok,
-      %State{current_number: current_number, stash_pid: stash_pid}}
+      %State{current_number: current_number, stash_pid: stash_pid}
+    }
   end
 
   def handle_call(:next_number, _from, state) do
     {:reply,
       state.current_number,
-      %{state | current_number: state.current_number + state.delta}}
+      %{state | current_number: state.current_number + state.delta}
+    }
   end
 
   def handle_cast({:increment_number, delta}, state) do
     {:noreply,
-      %{state | current_number: state.current_number + delta, delta: delta}}
+      %{state | current_number: state.current_number + delta, delta: delta}
+    }
   end
 
   def handle_cast(:clear, state) do
     {:noreply,
-      %{state | current_number: 0, delta: 1}}
+      %{state | current_number: 0, delta: 1}
+    }
   end
 
   def terminate(_reason, {current_number, stash_pid}) do
     Sequence.Stash.save_value stash_pid, current_number
   end
 
-  def format_status(_reason, [ _pdict, state ]) do
-    [data: [{'State', "My current state is '#{inspect state}', and I'm happy"}]]
+  def code_change("0", old_state = {current_number, stash_pid}, _extra) do
+    new_state = %State{
+      current_number: current_number,
+      stash_pid: stash_pid,
+      delta: 1
+    }
+
+    Logger.info "Changing code from 0 to 1"
+    Logger.info inspect(old_state)
+    Logger.info inspect(new_state)
+
+    {:ok, new_state}
   end
 end
